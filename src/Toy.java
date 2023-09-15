5a2efe745e534bb2543db801bfe0bbe564101ac8
aleksey.krasnov <krasnov.80@mail.ru>
2023-09-15 12:46:15 +0300

    Class Toy

---
+++ src/Toy.java
@@ -0,0 +1,11 @@
+public class Toy {
+    private long idToy;
+    private String nameToy;
+    private double lossRate;
+
+    public Toy(long idToy, String nameToy, double lossRate) {
+        this.idToy = idToy;
+        this.nameToy = nameToy;
+        this.lossRate = lossRate;
+    }
+}
